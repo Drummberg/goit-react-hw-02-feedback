79393b48c878326b3480c33b47890c160795b6ea
Drummberg <drummberg@gmail.com>
2022-05-06 01:19:36 +0300

    Update React

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Section from './components/Section/Section';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <>
+        <Section title="Please leave feedback" />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Section/Section.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Section.module.css';
+import PropTypes from 'prop-types';
+
+const Section = ({ title, children }) => {
+  return (
+    <section className={styles.reviews}>
+      <h2>{title}</h2>
+      {children}
+    </section>
+  );
+};
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+};
+
+export default Section;
