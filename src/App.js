b84d84db76db7da859e1d546b2700df1174269fb
Drummberg <drummberg@gmail.com>
2022-05-06 23:45:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Section from './components/Section/Section';
 import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
 import Statistics from './components/Statistics/Statistics';
-import Notification from './components/Notification/Notification';
 
 class App extends Component {
   state = {
