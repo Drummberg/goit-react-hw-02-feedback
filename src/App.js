35d619ddfb5ad27cca51b2bf876d61ed21734766
Drummberg <drummberg@gmail.com>
2022-05-06 22:55:41 +0300

    Update React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Section from './components/Section/Section';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
 import Notification from './components/Notification/Notification';
 
 class App extends Component {
@@ -9,14 +10,23 @@ class App extends Component {
     bad: 0,
   };
 
+  onBtnClick = feedback => {
+    this.setState(prevState => ({
+      [feedback]: prevState[feedback] + 1,
+    }));
+  };
+
   render() {
     const { good, neutral, bad } = this.state;
     const options = Object.keys(this.state);
 
     return (
       <>
-        <Section title="Please leave feedback" />
-        <Notification message="There is no feedback" />
+        <Section title="Please leave feedback">
+          <FeedbackOptions options={options} onBtnClick={this.onBtnClick} />
+
+          <Notification message="There is no feedback" />
+        </Section>
       </>
     );
   }
